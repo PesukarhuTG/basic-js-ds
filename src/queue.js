5d25394949ec5c11c3f024af0d538da25c5e377e
Tatiana <ganzhinovat@yandex.ru>
2022-04-30 22:39:05 +0300

    feat: add the solution of 'queue' task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if (!this.head) {
+      this.head = new ListNode(value); //если head не сущ-ет, создаем новый узел
+      this.tail = this.head; //узел head и tail одинаков
+    } else {
+      this.tail.next = new ListNode(value); //создаем новый узел конца
+      this.tail = this.tail.next; //передвигаем указатель конца
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const headValue = this.head.value; //извлекаем значение
+    this.head = this.head.next; //удаляем, перемещая head
+    return headValue;
   }
 }
 
